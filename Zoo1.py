401d3a2ec62d024596b50eaf8530e70b47541b50
Pistacja1212 <zuchowskaola4@gmail.com>
2023-09-29 20:28:23 +0200

    My First Commit

---
+++ Zoo1.py
@@ -0,0 +1,28 @@
+class zoo:
+    def __init__(self, name, food, occurance) -> None:
+        print('Nazwa:',name)
+        print('Pożywienie:',food)
+        print('Występowanie:',occurance)
+        print()
+
+cage1 = zoo(
+    'Pies',
+     'Trawa',
+    'Ameryka Południowa'
+    )
+cage2 = zoo(
+    'Lew',
+     'Mięso',
+    'Afryka'
+    )
+cage3 = zoo(
+    'Panda',
+     'Bambus',
+    'Chiny'
+    )
+cage4 = zoo(
+    'Żyrafa',
+     'Liście',
+    'Afryka'
+    )
+
